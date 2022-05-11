5f50ef46bcb9fb439ad973b2502e91c45ebc6623
CORP\47993 <preetys@hexaware.com>
2022-05-11 11:59:00 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Carousel, { CarouselItem } from "./Carousel";
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel>
+      <CarouselItem><img src={image1} alt={"image1"} className={"image1"}/></CarouselItem>
+      <CarouselItem><img src={image2} alt={"image1"} className={"image2"}/></CarouselItem>
+      <CarouselItem><img src={image3} alt={"image1"} className={"image3"}/></CarouselItem>
+      </Carousel>
     </div>
   );
 }
